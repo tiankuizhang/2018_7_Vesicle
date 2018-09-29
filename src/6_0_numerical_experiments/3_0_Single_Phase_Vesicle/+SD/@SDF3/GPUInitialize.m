2eb5b408db9dd397fc8eeb4934fd4cec42d3b1a9
Tiankui Zhang <tiankuizhang@email.arizona.edu>
2018-09-29 16:02:39 -0700

    red blood cell shaped added. convergence test for bending force added. force not converging

--- src/6_0_numerical_experiments/3_0_Single_Phase_Vesicle/+SD/@SDF3/GPUInitialize.m
+++ src/6_0_numerical_experiments/3_0_Single_Phase_Vesicle/+SD/@SDF3/GPUInitialize.m
@@ -1,7 +1,7 @@
 % initialize GPU functions
 function GPUInitialize(obj)
 	
-	obj.ThreadBlockSize = [obj.GD3.mrows,ceil(512/obj.GD3.mrows),1];
+	obj.ThreadBlockSize = [obj.GD3.mrows,floor(512/obj.GD3.mrows),1];
 	obj.GridSize = [ceil(obj.GD3.mrows/obj.ThreadBlockSize(1)), ...
 					ceil(obj.GD3.ncols/obj.ThreadBlockSize(2)), ...
 					ceil(obj.GD3.lshts/obj.ThreadBlockSize(3))];
---
+++ src/6_0_numerical_experiments/3_0_Single_Phase_Vesicle/p7_test_bendingforce_convergence.m
@@ -0,0 +1,31 @@
+% we shall test the convergence of various geometrical quantities
+% calculated with FDM from a level set approach to see if there is
+% any error in implementation
+
+%Size = [64,64,64];
+%Size = [96,96,96];
+%Size = [128,128,128];
+%Size = [192,192,192];
+Size = [256,256,256];
+[X,Y,Z,f,Nx,Ny,Nz,MC,SL_MC] = SD.Shape.RedBloodCell(Size,1.0);
+
+grid = SD.GD3(X,Y,Z);
+map = SD.SDF3(grid,X,Y,Z,f(X,Y,Z));
+map.F = map.WENORK3Reinitialization(map.F,100);
+map.GPUsetCalculusToolBox
+
+% theoretical Normal, MeanCurvature and Surface Laplacian of MeanCurvature
+Nx = Nx(X,Y,Z);
+Ny = Ny(X,Y,Z);
+Nz = Nz(X,Y,Z);
+MC = MC(X,Y,Z);
+SL_MC = SL_MC(X,Y,Z);
+
+diffMC = map.WENORK3Extend(MC - map.MeanCurvature,100);
+
+MeanCurvatureSurfaceLaplacian = map.SurfaceLaplacian(map.WENORK3Extend(map.MeanCurvature,100));
+diffSL_MC = map.WENORK3Extend(SL_MC - MeanCurvatureSurfaceLaplacian,100);
+
+MCL2Error = sqrt(map.surfaceIntegral(diffMC.^2))
+SLMCL2Error = sqrt(map.surfaceIntegral(diffSL_MC.^2))
+
